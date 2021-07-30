c89ef8532d608693f16c6321b406d2d2f3e01c73
paikwiki <paikwiki@gmail.com>
2021-07-30 12:09:22 +0900

    Add log 20210730-120922

--- run.sh
+++ run.sh
@@ -3,4 +3,12 @@
 SOURCE_FOLDER="/home/pi/codes/collector-n-lighter/logs/"
 TARGET_FOLDER="/home/pi/codes/log-writer/"
 
+cd $TARGET_FOLDER
+
 cp -r $SOURCE_FOLDER $TARGET_FOLDER
+
+git add .
+git commit -m "Add log $(date '+%Y%m%d-%H%M%S')"
+git push
+
+cd -
